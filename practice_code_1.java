8d2d561befda1d0df91d5653bc1fe8dc3a64bc51
Gaurav Sharma <kgauravs192@gmail.com>
2023-10-10 23:35:08 +0530

    Prime Number

---
+++ practice_code_1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class practice_code_1 {
+    public static void main(String[] args) {
+        int j=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number To check Prime or Not");
+        int num = sc.nextInt();
+
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                j++;
+            }
+        }
+        if(j==2)
+            System.out.println("Prime: "+num);
+        else
+            System.out.println("Not Prime: "+num);
+            sc.close();
+    }
+    
+}
